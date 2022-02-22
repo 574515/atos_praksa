5a1d8979372f55cf785a0401ed90423fea8a55b0
574515 <574515c0d1ng@gmail.com>
2022-02-22 22:36:22 +0100

    Seventh task done

---
+++ zadatak7/Professor/src/Professor.cpp
@@ -0,0 +1,5 @@
+#include <iostream> 
+#include <cstring>
+#include "Professor.h"
+
+Professor::Professor(std::string name){ professorName = name; }
---
+++ zadatak7/Student/src/Student.cpp
@@ -0,0 +1,5 @@
+#include <iostream> 
+#include <cstring>
+#include "Student.h"
+
+Student::Student(std::string name){ studentName = name; }
